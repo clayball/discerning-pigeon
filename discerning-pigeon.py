1c07824c9a9d0c05d8f0625e4ce66442f02b1d03
Clay Wells <cwells@cwells.org>
2017-01-30 16:02:06 -0500

    adding skeleton code.

---
+++ discerning-pigeon.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+
+# Discerning Pigeon
+# =================
+#
+#
+# Clay Wells <cwells at cwells.org>
+# 
+
+# ######### IMPORTS #########
+import feedparser
+import re
+
+
+
+# ######### VARIABLES #########
+
+
+
+# ######### FUNCTIONS #########
+
+
+
+
+
+# ######### MAIN PROGRAM #########
+
+
+
+
